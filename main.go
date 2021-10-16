82ae3d589b9ea7edf632f9d27c61703ee6c50645
William Ku <kucheech@gmail.com>
2021-10-16 11:27:27 +0800

    * enable cors allow all origins for GET

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/patrickmn/go-cache"
 	"time"
@@ -22,6 +23,11 @@ func main() {
 	cache := cache.New(5*time.Minute, 10*time.Minute)
 
 	router := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowMethods = []string{"GET"}
+	router.Use(cors.New(config))
+
 	router.GET("/data", CacheCheck(cache), getDataByIp)
 
 	router.Run("localhost:8080")
